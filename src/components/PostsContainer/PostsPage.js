67b27dd438c5a5f5da083f5d2d55684ffcfc7d67
Josue Rodriguez <smexyjosue@gmail.com>
2020-01-21 13:14:46 -0800

    Add imports on App.js, set up state for data
    imports for App.js have been inputted in order to get the searchbar and
    postpage to show. Also State for data has been set to pass data along
    the children.

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -6,11 +6,8 @@ import "./Posts.css";
 // pass the data from App.js down as props then map through the data
 const PostsPage = () => {
   return (
-    <div className="posts-container-wrapper">
-      {/* map through data here */}
-    </div>
+    <div className="posts-container-wrapper">{/* map through data here */}</div>
   );
 };
 
 export default PostsPage;
-
